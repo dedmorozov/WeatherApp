ae4d3e40e9e08b701812867350472099133120e0
vamonospest <61283777+vamonospest@users.noreply.github.com>
2020-03-24 19:35:47 +0200

    Update weather.js

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,13 +1,33 @@
 const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+
+const updateUI = (data) => {
+
+    const cityDets = data.cityDets;
+    const weather = data.weather;
+
+    // обновления шаблона деталей
+    details.innerHTML = `
+    <h5 class="my-3">${cityDets.EnglishName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <div class="display-4 my-4">
+      <span>${Math.round(weather.Temperature.Metric.Value)}</span>
+      <span>&deg;C</span>
+    </div>
+    `;
+
+    //удаление d-none при наличии
+    if(card.classList.contains('d-none')){
+    card.classList.remove('d-none');
+    }
+};
+
 const updateCity = async (city) => {
 
-    const cityDetails = await getCity(city);
-    const weather = await getWeather(cityDetails.Key);
-
-    return {
-        cityDetails: cityDetails,
-        weather: weather
-    };
+    const cityDets = await getCity(city);
+    const weather = await getWeather(cityDets.Key);
+    return { cityDets, weather };
 };
 
 cityForm.addEventListener('submit', e => {
@@ -19,6 +39,7 @@ cityForm.addEventListener('submit', e => {
     cityForm.reset();
 
     // обновление ui с новым городом
-    updateCity(city).then(data => console.log(data))
+    updateCity(city)
+    .then(data => updateUI(data))
     .catch(err => console.log(err));
 });
